74c9936b7fa525599a4266daf1d9a7206c4b9e3d
Chris <rodcee13@gmail.com>
2023-11-02 13:11:45 -0400

    Parallel data fetching

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,11 +1,24 @@
 import getUser from "@/app/libs/getUser"
+import getUserPosts from "@/app/libs/getUserPosts"
 
-export default async function UserPage({params: {id}}) {
-    const userData = await getUser(id)
+export default async function UserPage({ params: { id } }) {
+    // initiate both request in parallel
+    const userData = getUser(id)
+    const userPosts = getUserPosts(id)
 
-  return (
-    <div>
-        <h1 className="text-5xl font-bold">{userData.name}</h1>
-    </div>
-  )
+    const [user, posts] = await Promise.all([userData, userPosts])
+
+    return (
+        <div>
+            <h1 className="text-5xl font-bold">UserInformation</h1>
+            <p className="text-3xl mt-10">{user.name}</p>
+            {posts.map(post => {
+                return (
+                    <p className="text-3xl mt-10">
+                        {post.title}
+                    </p>
+                )
+            })}
+        </div>
+    )
 }
